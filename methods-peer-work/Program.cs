0ac6d77682e7b4e3d3cbc02d62a32a9ce66536f0
JJknight64 <jwd8744@rit.edu>
2024-11-03 20:43:56 -0500

    created both variables and initialized the one for this account to 64

--- methods-peer-work/Program.cs
+++ methods-peer-work/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            // side note: i couldn't get a hold of my partner for this project, so i'll be working from two different clients on different computers. i'll provide screenshots of my progess as proof when i submit this to myCourses
+
+            int jacob1FavNumber;
+            int jacob2FavNumber;
+
+            //initialize
+
+            jacob1FavNumber = 64;
         }
     }
 }
